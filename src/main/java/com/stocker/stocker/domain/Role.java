cfe3b3c4f53ee47776ba60964932c80800037390
kyso8575 <kyso8575@gmail.com>
2025-04-29 20:26:14 +0800

    Recreate Project

--- src/main/java/com/stocker/stocker/domain/Role.java
+++
@@ -1,9 +0,0 @@
-package com.stocker.stocker.domain;
-
-/**
- * 사용자 역할을 정의하는 열거형
- */
-public enum Role {
-    USER,  // 일반 사용자
-    ADMIN  // 관리자
-} 
---
+++ src/main/java/com/stocker_back/stocker_back/StockerBackApplication.java
@@ -0,0 +1,13 @@
+package stocker_back.stocker_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockerBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockerBackApplication.class, args);
+	}
+
+}
